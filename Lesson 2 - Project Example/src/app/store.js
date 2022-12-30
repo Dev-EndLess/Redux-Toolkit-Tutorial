8f5d80b5fc70dc57112263e9c8cde9772b8a9c40
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-30 09:21:17 +0100

    added Project example

---
+++ Lesson 2 - Project Example/src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import postsSliceReducer from '../features/posts/postsSlice'
+import usersSliceReducer from '../features/users/usersSlice'
+
+export const store = configureStore({
+  reducer: {
+    posts: postsSliceReducer,
+    users: usersSliceReducer
+  }
+})
